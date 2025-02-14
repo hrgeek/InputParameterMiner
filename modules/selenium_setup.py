5050b8e3681c1a0147e38b83ecc1016967af0ef0
Hadi Rezaee <32469089+hrgeek@users.noreply.github.com>
2025-02-15 01:14:05 +0330

    Update selenium_setup.py

--- modules/selenium_setup.py
+++ modules/selenium_setup.py
@@ -1,13 +1,9 @@
-from selenium import webdriver
+from seleniumwire import webdriver as wired_webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from seleniumwire import webdriver as wired_webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 def setup_selenium(url):
-    """Set up Selenium with ChromeDriver."""
+    """Set up Selenium with ChromeDriver to fetch JavaScript-rendered content."""
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
@@ -29,7 +25,6 @@ def setup_selenium(url):
             seleniumwire_options=seleniumwire_options
         )
         driver.get(url)
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
         return driver
     except Exception as e:
         print(f"Error setting up Selenium: {e}")
